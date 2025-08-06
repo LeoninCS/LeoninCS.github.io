4dc534c4635b19492ab232ccbb3f3a7344e33e45
LeoninCS <xianchaoqian@gmail.com>
2025-08-06 20:16:48 +0800

    fix: use root base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/LeoninCS.github.io/',  // 仓库名要与 GitHub 仓库保持一致
+  base: '/', // 👈 改成这个
 })
