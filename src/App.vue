d757fa96ee31a138846d05c3aa7e78b8162245bf
LeoninCS <xianchaoqian@gmail.com>
2025-08-06 19:51:36 +0800

    自动化部署

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <HelloWorld/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue' // 假设组件路径正确
+
+export default {
+  components: {
+    HelloWorld // 注册组件
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,6 @@
+<template> 
+  <h1>Absurdity is a start!</h1>
+</template>
+<script>
+
+</script>
